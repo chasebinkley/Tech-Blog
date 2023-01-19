7bc1d45be98203ade1d79d24080a4576561e3e5a
chasebinkley <chasebinkley@yahoo.com>
2023-01-18 20:28:22 -1000

    creates gitignore and sets up server.js

--- server.js
+++ server.js
@@ -1,30 +1,34 @@
 const express = require("express");
 const session = require("express-session");
 const exphbs = require("express-handlebars");
-const routes = require('./controllers/index');
+const routes = require("./controllers/index");
+const sequelize = require("./config/connection");
+const SequelizeStore = require("connect-session-sequelize")(session.Store);
 
-
-const sessionSettings = {
-    secret: "786878y8", 
-    cookie:{}, 
-    resave:false, 
-    saveUnintitalized:true
-}
-const PORT = process.env.PORT || 3001
+const sess = {
+  secret: "Super secret secret",
+  cookie: {},
+  resave: false,
+  saveUninitialized: true,
+  store: new SequelizeStore({
+    db: sequelize,
+  }),
+};
+const PORT = process.env.PORT || 3001;
 const app = express();
 const hbs = exphbs.create();
 
-
 app.engine("handlebars", hbs.engine);
-app.set('view engine', 'handlebars')
-app.use(express.json())
-app.use(express.static("public"))
-app.use(express.urlencoded({extended:true}))
-app.use(session(sessionSettings))
+app.set("view engine", "handlebars");
+app.use(express.json());
+app.use(express.static("public"));
+app.use(express.urlencoded({ extended: true }));
+app.use(session(sess));
 
+app.use(routes);
 
-app.use(routes)
-
-app.listen(PORT, () => {
-    console.log("server is running")
-})
+sequelize.sync({ force: false }).then(() => {
+  app.listen(PORT, () => {
+    console.log("server is running");
+  });
+});
