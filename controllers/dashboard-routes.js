3f1d8111f525ff76c7e29ced9fe3e5b1fa586bc5
chasebinkley <chasebinkley@yahoo.com>
2023-01-18 21:27:46 -1000

    sets up the routes

---
+++ controllers/dashboard-routes.js
@@ -0,0 +1,18 @@
+const router = require("express").Router();
+const { Post, Comment, User } = require("../models/");
+const withAuth = require("../utils/auth");
+
+router.get("/", async (req, res) => {
+  try {
+    const postData = await Post.findAll({
+      
+    });
+
+    const posts = postData.map((post) => post.get({ plain: true }));
+    res.render("post", { posts, layout: "dashboard", style: "dashboard.css" });
+  } catch (err) {
+    res.json(err);
+  }
+});
+
+module.exports = router;
---
+++ controllers/home-routes.js
@@ -0,0 +1,29 @@
+const router = require("express").Router();
+const { Post, Comment, User } = require("../models/");
+const withAuth = require("../utils/auth");
+
+router.get("/", async (req, res) => {
+  try {
+    const postData = await Post.findAll({});
+    const posts = postData.map((post) => post.get({ plain: true }));
+    res.render("home1", { posts, layout: "home" });
+  } catch (err) {
+    res.status(400).json(err);
+  }
+});
+
+router.get("/login", (req, res) => {
+  if (req.session.loggedIn) {
+    res.redirect("/");
+  }
+  res.render("login", { layout: "home" });
+});
+
+router.get("/signup", (req, res) => {
+  if (req.session.loggedIn) {
+    res.redirect("/");
+  }
+  res.render("signup", { layout: "home" });
+});
+
+module.exports = router;
